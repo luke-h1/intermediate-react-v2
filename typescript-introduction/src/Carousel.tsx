443fca19fee712fb7dd748fdba6e51a67b30fe6a
luke-h1 <luke.howsam@yahoo.com>
2020-10-01 20:42:24 +0100

    add typescript lessons

---
+++ typescript-introduction/src/Carousel.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Photo } from "@frontendmasters/pet";
+
+interface Iprops {
+  media: Photo[];
+}
+
+interface Istate {
+  active: number;
+  photos: string[];
+}
+
+class Carousel extends React.Component<Iprops, Istate> {
+  public state = {
+    photos: [],
+    active: 0
+  };
+  public static getDerivedStateFromProps({ media }: Iprops) {
+    let photos = ["http://placecorgi.com/600/600"];
+
+    if (media.length) {
+      photos = media.map(({ large }) => large);
+    }
+
+    return { photos };
+  }
+  public handleIndexClick = (event: React.MouseEvent<HTMLElement>) => {
+    if (!(event.target instanceof HTMLElement)) {
+      return;
+    }
+    if (event.target.dataset.index) {
+      this.setState({
+        active: +event.target.dataset.index
+      });
+    }
+  };
+  public render() {
+    const { photos, active } = this.state;
+    return (
+      <div className="carousel">
+        <img src={photos[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => (
+            <img
+              key={photo}
+              onClick={this.handleIndexClick}
+              data-index={index}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+export default Carousel;
